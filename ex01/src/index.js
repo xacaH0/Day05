814bce870157f1347d2ed697751ab8df887b42dd
unknown <xacah364@gmail.com>
2021-03-12 19:16:55 +0100

    axxaxaxa

---
+++ ex01/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+
+var myFirstString = "I think therefore I \"am\" -- sentient.";
+var mySecondString = "No, you only \"think\", so you aren't.";        
+        
+        return {
+            myFirstString,
+            mySecondString
+            
+        };
+    }
+        
+     console.log(main());
+        
+     module.exports = main;
